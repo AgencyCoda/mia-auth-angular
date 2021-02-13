4343c9acc53ce38d787ca87ab0b3cbc336ec9334
Matias Camiletti <matias.camiletti@gmail.com>
2021-02-13 12:11:48 -0300

    Create Auth Config.

--- projects/agencycoda/mia-auth/src/lib/mia-auth.module.ts
+++ projects/agencycoda/mia-auth/src/lib/mia-auth.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Injectable, InjectionToken, NgModule } from '@angular/core';
 import { MiaAuthComponent } from './mia-auth.component';
 
+export const MIA_AUTH_PROVIDER = new InjectionToken<MiaAuthConfig>('agencycoda.auth');
 
+@Injectable()
+export class MiaAuthConfig {
+  baseUrl: string = '';
+}
 
 @NgModule({
   declarations: [MiaAuthComponent],
-  imports: [
-  ],
-  exports: [MiaAuthComponent]
+  imports: [],
+  exports: [MiaAuthComponent],
+  providers: [
+    {
+      provide: MIA_AUTH_PROVIDER,
+      useClass: MiaAuthConfig
+    }
+  ]
 })
 export class MiaAuthModule { }
--- projects/agencycoda/mia-auth/src/lib/mia-auth.service.ts
+++ projects/agencycoda/mia-auth/src/lib/mia-auth.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { MiaAuthConfig, MIA_AUTH_PROVIDER } from './mia-auth.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MiaAuthService {
 
-  constructor() { }
+  constructor(
+    @Inject(MIA_AUTH_PROVIDER) protected config: MiaAuthConfig
+  ) { }
+
+  getConfig() {
+    return this.config;
+  }
 }
